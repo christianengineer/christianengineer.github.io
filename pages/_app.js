b9b110c795c35a3bb545702826b14bac8346c9f5
Christian Ipanaque <christian.ipanaque@formidable.com>
2021-01-20 00:21:43 -0800

    add fonts and globalStyles

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,68 @@
-import "../css/index.css";
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import theme from '../src/styles/theme';
+// import '../css/index.css';
 
-export default ({ Component, pageProps }) => <Component {...pageProps} />;
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Oswald';
+    font-style: normal;
+    font-weight: 400;
+    src: local(''),
+      url('./fonts/oswald-v24-latin-regular.woff2') format('woff2'),
+      /* Chrome 26+, Opera 23+, Firefox 39+ */
+        url('./fonts/oswald-v24-latin-regular.woff') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+  }
+  /* oswald-300 - latin */
+  @font-face {
+    font-family: 'Oswald';
+    font-style: normal;
+    font-weight: 300;
+    src: local(''), url('./fonts/oswald-v24-latin-300.woff2') format('woff2'),
+      /* Chrome 26+, Opera 23+, Firefox 39+ */
+        url('./fonts/oswald-v24-latin-300.woff') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+  }
+
+  html {
+    background-color: ${({ theme }) => theme.colors.primaryColor};
+    box-sizing: border-box;
+    font-size: 62.5%;
+  }
+
+  body {
+    font-family: ${({ theme }) => theme.fonts.primaryFont};
+  }
+
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+
+  body,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  ul,
+  ol,
+  li,
+  p,
+  pre,
+  blockquote,
+  figure,
+  hr {
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+export default function App({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
---
+++ src/styles/theme.js
@@ -0,0 +1,16 @@
+const colors = {
+  primaryColor: '#3a3729',
+  secondaryColor: '#fbfbfb',
+  lightGrayColor: '#D8D8D8',
+  accentColor: '#fff95a',
+  accentSecondaryColor: '#898632',
+};
+
+const fonts = {
+  primaryFont: `'Oswald', 'sans-serif'`,
+};
+
+export default {
+  colors,
+  fonts,
+};
