74c5833a1f820a8cc8c062304be90f25da801938
Christian Ipanaque <christian.ipanaque@formidable.com>
2021-01-23 11:57:15 -0800

    add animations

--- pages/_app.js
+++ pages/_app.js
@@ -55,6 +55,52 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
   }
+
+  @keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+
+@keyframes slideInFromLeft {
+  0% {
+    transform: translateX(-100%);
+  }
+  100% {
+    transform: translateX(0);
+  }
+}
+
+@keyframes slideInFromRight {
+  0% {
+    transform: translateX(300%);
+  }
+  100% {
+    transform: translateX(0);
+  }
+}
+
+@keyframes slideInFromBottom {
+  0% {
+    transform: rotate(-90deg) translateX(-100%);
+  }
+  100% {
+    transform: rotate(-90deg) translateX(0);
+  }
+}
+
+@keyframes slideInFromTop {
+  0% {
+    transform: rotate(-90deg) translateX(200%);
+  }
+  100% {
+    transform: rotate(-90deg) translateX(0);
+  }
+}
+
 `;
 
 export default function App({ Component, pageProps }) {
