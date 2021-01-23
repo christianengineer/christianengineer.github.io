2b5c189b6339b60f2fed5cf54d2104f5e307f688
Christian Ipanaque <christian.ipanaque@formidable.com>
2021-01-23 15:17:30 -0800

    add education section

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,75 @@
 import Header from '../src/components/Header';
 import Nav from '../src/components/Nav';
+import Section from '../src/components/Section';
 
 const LandingPage = () => {
   return (
-    <Header>
-      <Nav top>
-        <div>
-          <Nav.Link href="https://github.com/chrisipanaque" target="_blank">
-            Github
-          </Nav.Link>
-        </div>
-        <div>
-          <Nav.Link
-            href="https://www.linkedin.com/in/chrisipanaque/"
-            target="_blank"
-          >
-            LinkedIn
-          </Nav.Link>
-        </div>
-        <div>
-          <Nav.Link href="./christian_ipanaque_2019.pdf" target="_blank">
-            Resume
-          </Nav.Link>
-        </div>
-      </Nav>
-      <Header.Content left>
-        <h1>Christian</h1>
-        <h2>Ipanaque</h2>
-      </Header.Content>
-      <Header.Content right />
-      <Nav bottom>
-        <div>
-          <Nav.Link href="#education">Education</Nav.Link>
-        </div>
-        <div>
-          <Nav.Link href="#achievements">Achievements</Nav.Link>
-        </div>
-        <div>
-          <Nav.Link href="#projects">Projects</Nav.Link>
-        </div>
-      </Nav>
-    </Header>
+    <>
+      <Header>
+        <Nav top>
+          <div>
+            <Nav.Link href="https://github.com/chrisipanaque" target="_blank">
+              Github
+            </Nav.Link>
+          </div>
+          <div>
+            <Nav.Link
+              href="https://www.linkedin.com/in/chrisipanaque/"
+              target="_blank"
+            >
+              LinkedIn
+            </Nav.Link>
+          </div>
+          <div>
+            <Nav.Link href="./christian_ipanaque_2019.pdf" target="_blank">
+              Resume
+            </Nav.Link>
+          </div>
+        </Nav>
+        <Header.Content left>
+          <h1>Christian</h1>
+          <h2>Ipanaque</h2>
+        </Header.Content>
+        <Header.Content right />
+        <Nav bottom>
+          <div>
+            <Nav.Link href="#education">Education</Nav.Link>
+          </div>
+          <div>
+            <Nav.Link href="#achievements">Achievements</Nav.Link>
+          </div>
+          <div>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+          </div>
+        </Nav>
+      </Header>
+
+      <main>
+        <Section
+          backgroundGradient="education"
+          align="right"
+          sectionTheme="dark"
+          linkTheme="dark"
+        >
+          <h2>Education</h2>
+          <div>
+            <h3>Lambda School</h3>
+            <h4>
+              <a href="https://lambdaschool.com/curriculum" target="_blank">
+                View Curriculum
+              </a>
+            </h4>
+            <h4>
+              9-month Full Stack Software Development and Computer Science
+              Bootcamp that provides a full-time immersive hands-on training
+              curriculum. Received training in cross-functional team
+              collaboration, and constantly going above and beyond in meeting
+              code standards.
+            </h4>
+          </div>
+        </Section>
+      </main>
+    </>
   );
 };
 
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,5 +1,73 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-const Section = styled.section``;
+const backgroundGradients = {
+  education: css`
+    background: linear-gradient(rgba(58, 55, 41, 0.8), rgba(58, 55, 41, 0.8)),
+      url(./images/christian-ipanaque-education.jpg);
+  `,
+};
+
+const sectionThemes = {
+  dark: {
+    h2: css`
+      color: ${({ theme }) => theme.colors.accentColor};
+    `,
+    h3: css`
+      color: ${({ theme }) => theme.colors.secondaryColor};
+    `,
+    h4: css`
+      color: ${({ theme }) => theme.colors.lightGrayColor};
+    `,
+  },
+};
+
+const linkThemes = {
+  light: css`
+    text-decoration: underline;
+    font-weight: 400;
+    color: ${({ theme }) => theme.colors.primaryColor};
+  `,
+  dark: css`
+    text-decoration: underline;
+    font-weight: 400;
+    color: ${({ theme }) => theme.colors.accentColor};
+  `,
+};
+
+const Section = styled.section`
+  text-align: ${({ align }) => align};
+  position: relative;
+  display: flex;
+
+  flex-direction: column;
+  padding: 10%;
+  ${({ backgroundGradient }) => backgroundGradients[backgroundGradient]}
+  background-size: cover;
+
+  div {
+    margin-bottom: 40px;
+  }
+
+  h2 {
+    font-size: 10vw;
+    text-transform: uppercase;
+    ${({ sectionTheme }) => sectionThemes[sectionTheme]['h2']}
+  }
+
+  h3 {
+    font-size: 5vw;
+    ${({ sectionTheme }) => sectionThemes[sectionTheme]['h3']}
+  }
+
+  h4 {
+    font-size: 3vw;
+    font-weight: 300;
+    ${({ sectionTheme }) => sectionThemes[sectionTheme]['h4']}
+  }
+
+  a {
+    ${({ linkTheme }) => linkThemes[linkTheme]}
+  }
+`;
 
 export default Section;
