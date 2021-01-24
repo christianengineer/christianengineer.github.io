5aad29874ef2a2ee9521149c1bc55ea7aad18098
Christian Ipanaque <christian.ipanaque@formidable.com>
2021-01-23 16:20:59 -0800

    add Head in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { ThemeProvider } from 'styled-components';
 import theme from '../src/styles/theme';
 import GlobalStyle from '../src/styles/global-style';
@@ -6,6 +7,12 @@ export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <Head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>Christian Ipanaque - Software Engineer in Seattle, WA</title>
+      </Head>
       <Component {...pageProps} />
     </ThemeProvider>
   );
--- pages/_document.js
+++ pages/_document.js
@@ -5,13 +5,6 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <meta charset="UTF-8" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-          <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-          <title>Christian Ipanaque - Software Engineer in Seattle, WA</title>
           <link
             rel="preload"
             as="font"
